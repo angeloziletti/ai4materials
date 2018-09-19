1cbe5f8007887b829650af6fa9778bd2b5bd7422
Angelo Ziletti <ziletti@fhi-berlin.mpg.de>
2018-09-19 10:24:34 +0200

    Remove theano

--- setup.py
+++ setup.py
@@ -79,10 +79,9 @@ def main():
         # https://packaging.python.org/en/latest/requirements.html
         install_requires=['ase==3.15.0',  # neighbors list does not work for ase 3.16
             'scikit-learn >=0.17.1', 'tensorflow==1.8.0', 'pint', 'future', 'pandas', 'bokeh==0.11.0',
-            'enum34', 'pymatgen', 'keras==1.2.0', 'pillow>=2.7.0', 'mendeleev', 'sqlalchemy', 'keras-tqdm',
-            'theano==0.9.0', 'weave', 
+            'enum34', 'pymatgen', 'keras==1.2.0', 'pillow>=2.7.0', 'mendeleev', 'sqlalchemy', 'keras-tqdm', 'weave', 
             'seaborn', 'paramiko', 'multiprocessing', 'scipy', 'nose>=1.0'],
-#'mayavi', 'numpy', 'h5py', 'cython>=0.19', 'hdbscan'],
+#'theano==0.9.0', 'mayavi', 'numpy', 'h5py', 'cython>=0.19', 'hdbscan'],
 
         #setup_requires=['nomadcore', 'atomic_data'],
         # List additional groups of dependencies here (e.g. development
