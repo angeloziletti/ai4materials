91783705d7347def2375f1c0c85388711387c511
Angelo Ziletti <ziletti@fhi-berlin.mpg.de>
2018-09-19 10:56:37 +0200

    Add mock for ai4materials

--- docs/conf.py
+++ docs/conf.py
@@ -22,25 +22,26 @@ import sys
 atomic_data_dir = os.path.abspath('../../atomic-data')
 sys.path.insert(0, atomic_data_dir)
 
-
 # -- Mock imports since readthedocs cannot compile C-based code --------
 
 import sys
 from mock import Mock as MagicMock
+
 
 class Mock(MagicMock):
     @classmethod
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['ase', 'scikit-learn', 'tensorflow', 'pint', 'future', 'pandas', 'bokeh',
-		'enum34', 'pymatgen', 'keras', 'pillow', 'mendeleev', 'keras-tqdm', 'weave',
-		'seaborn', 'paramiko', 'multiprocessing', 'scipy', 'nose', 'sqlalchemy', 'theano', 
-		'mayavi', 'numpy', 'h5py', 'cython', 'hdbscan']
 
+MOCK_MODULES = ['ai4materials', 'ai4materials.dataprocessing', 'ai4materials.descriptors',
+                'ai4materials.interpretation', 'ai4materials.models', 'ai4materials.utils',
+                'ai4materials.visualization', 'ase', 'scikit-learn', 'tensorflow', 'pint', 'future', 'pandas', 'bokeh',
+                'enum34', 'pymatgen', 'keras', 'pillow', 'mendeleev', 'keras-tqdm', 'weave', 'seaborn', 'paramiko',
+                'multiprocessing', 'scipy', 'nose', 'sqlalchemy', 'theano', 'mayavi', 'numpy', 'h5py', 'cython',
+                'hdbscan']
 
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
-
 
 # -- General configuration ------------------------------------------------
 
@@ -51,14 +52,9 @@ sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages']
-#,
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo', 'sphinx.ext.coverage',
+              'sphinx.ext.mathjax', 'sphinx.ext.viewcode', 'sphinx.ext.githubpages']
+# ,
 #    'numpydoc',
 #    'nbsphinx']
 
@@ -84,8 +80,8 @@ author = u'Angelo Ziletti, Fritz Haber Institute of the Max Planck Society'
 
 # If true, sectionauthor and moduleauthor directives will be shown in the
 # output. They are ignored by default.
-#show_authors = False
-show_authors=True
+# show_authors = False
+show_authors = True
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -118,15 +114,14 @@ pygments_style = 'sphinx'
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
 
-
 # -- Options for HTML output ----------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'alabaster'
-#html_theme = 'bizstyle'
-#html_theme = 'classic'
-#html_theme = 'cloud'
+# html_theme = 'alabaster'
+# html_theme = 'bizstyle'
+# html_theme = 'classic'
+# html_theme = 'cloud'
 # needs pip install sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
 # html_theme = 'alabaster'
@@ -135,7 +130,7 @@ html_theme = "sphinx_rtd_theme"
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-#html_theme_options = {}
+# html_theme_options = {}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -148,21 +143,15 @@ html_static_path = ['_static']
 # This is required for the alabaster theme
 # refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
 html_sidebars = {
-    '**': [
-        'about.html',
-        'navigation.html',
-        'relations.html',  # needs 'show_related': True theme option to display
-        'searchbox.html',
-        'donate.html',
-    ]
-}
+    '**': ['about.html', 'navigation.html', 'relations.html',  # needs 'show_related': True theme option to display
+        'searchbox.html', 'donate.html', ]}
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
 html_title = u'ai4materials v0.1.0'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+# html_short_title = None
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
@@ -171,7 +160,7 @@ html_logo = 'ai_for_materials_logo.png'
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+# html_favicon = None
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -181,29 +170,29 @@ html_static_path = ['_static']
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
-#html_extra_path = []
+# html_extra_path = []
 
 # If not None, a 'Last updated on:' timestamp is inserted at every page
 # bottom, using the given strftime format.
 # The empty string is equivalent to '%b %d, %Y'.
-#html_last_updated_fmt = None
+# html_last_updated_fmt = None
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
-#html_use_smartypants = True
+# html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
+# html_sidebars = {}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
-#html_additional_pages = {}
+# html_additional_pages = {}
 
 # If false, no module index is generated.
-#html_domain_indices = True
+# html_domain_indices = True
 
 # If false, no index is generated.
-#html_use_index = True
+# html_use_index = True
 
 # If true, the index is split into individual pages for each letter.
 html_split_index = True
@@ -222,11 +211,9 @@ html_show_copyright = True
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'ai4materials-doc'
 
-
 # -- Options for LaTeX output ---------------------------------------------
 
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
+latex_elements = {# The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
 
@@ -246,32 +233,18 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, 'ai4materials.tex', u'ai4materials Documentation',
-     u'Angelo Ziletti', 'manual'),
-]
-
+latex_documents = [(master_doc, 'ai4materials.tex', u'ai4materials Documentation', u'Angelo Ziletti', 'manual'), ]
 
 # -- Options for manual page output ---------------------------------------
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'ai4materials', u'ai4materials Documentation',
-     [author], 1)
-]
-
+man_pages = [(master_doc, 'ai4materials', u'ai4materials Documentation', [author], 1)]
 
 # -- Options for Texinfo output -------------------------------------------
 
 # Grouping the document tree into Texinfo files. List of tuples
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
-texinfo_documents = [
-    (master_doc, 'ai4materials', u'ai4materials Documentation',
-     author, 'ai4materials', 'Artificial intelligence for materials science',
-     'Miscellaneous'),
-]
-
-
-
+texinfo_documents = [(master_doc, 'ai4materials', u'ai4materials Documentation', author, 'ai4materials',
+                      'Artificial intelligence for materials science', 'Miscellaneous'), ]
