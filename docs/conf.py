7814be7e532c38f033bf98c958225a0edb07a285
Angelo Ziletti <ziletti@fhi-berlin.mpg.de>
2018-09-19 12:10:08 +0200

    Use keyword for mock

--- docs/conf.py
+++ docs/conf.py
@@ -28,16 +28,8 @@ sys.path.insert(0, ai4materials_dir)
 # -- Mock imports since readthedocs cannot compile C-based code --------
 
 import sys
-from mock import Mock as MagicMock
 
-
-class Mock(MagicMock):
-    @classmethod
-    def __getattr__(cls, name):
-        return MagicMock()
-
-
-MOCK_MODULES = ['ai4materials',
+autodoc_mock_imports = ['ai4materials',
 		'ai4materials.wrappers',
 		'ai4materials.descriptors',
 		#'ai4materials.',
@@ -52,8 +44,6 @@ MOCK_MODULES = ['ai4materials',
                 'enum34', 'pymatgen', 'keras', 'pillow', 'mendeleev', 'keras-tqdm', 'weave', 'seaborn', 'paramiko',
                 'multiprocessing', 'scipy', 'nose', 'sqlalchemy', 'theano', 'mayavi', 'numpy', 'h5py', 'cython',
                 'hdbscan']
-
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
 
