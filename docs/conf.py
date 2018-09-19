f54b83e4d646d598842864e9952295418450f0ab
Angelo Ziletti <ziletti@fhi-berlin.mpg.de>
2018-09-19 09:29:54 +0200

    Remove numpydoc from doc dependencies.

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'numpydoc',
+#    'numpydoc',
     'nbsphinx']
 
 # Add any paths that contain templates here, relative to this directory.
